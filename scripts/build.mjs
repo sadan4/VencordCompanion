8f04fc9fb7eb26eed6d48dbbdd13c577cb0226af
sadan <117494111+sadan4@users.noreply.github.com>
2025-02-04 10:26:23 -0500

    update build and ignore

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,9 +1,14 @@
 import esbuild from "esbuild";
 import { commonOpts, webviewOpts } from "./common.mjs";
+import { writeFile } from "fs/promises";
 
 //@ts-check
 
 await Promise.all([
-    esbuild.build(commonOpts),
     esbuild.build(webviewOpts)
 ])
+const res = await esbuild.build({...commonOpts, 
+        sourcemap: "linked",
+        metafile: true
+    });
+await writeFile("dist/meta.json", JSON.stringify(res.metafile));

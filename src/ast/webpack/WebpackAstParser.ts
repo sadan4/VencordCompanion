205e77a0417a1e8d6ca53287239c489039221adb
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-17 18:26:53 -0400

    formatting

--- src/ast/webpack/WebpackAstParser.ts
+++ src/ast/webpack/WebpackAstParser.ts
@@ -326,53 +326,53 @@ export class WebpackAstParser extends AstParser {
         const where = await this.getModulesThatRequireThisModule();
         const locs: Location[] = [];
 
-        // TODO: support jumping from object literals
-        for (const [exportedName] of Object.entries(moduleExports)
+        const exportedNames = Object.entries(moduleExports)
             .filter(([, v]) => Array.isArray(v) && v.some((x) => {
                 if (!x)
                     return;
                 return x.contains(position);
-            }))) {
+            }));
+
+
+        // TODO: support jumping from object literals
+        for (const [exportedName] of exportedNames) {
             const seen: Record<string, Set<String>> = {};
 
-      // the module id that is being searched for uses
-      // the ID of the module that exportName will be imported from
-      // the exported name to search
-      type ElementType = [
-        moduleId: string,
-        importedId: string,
-        exportName: string | symbol,
-      ];
+            // the module id that is being searched for uses
+            // the ID of the module that exportName will be imported from
+            // the exported name to search
+            type ElementType = [
+                moduleId: string,
+                importedId: string,
+                exportName: string | symbol,
+            ];
 
-      const left: ElementType[]
-        = where?.sync.map((x) => [x, this.moduleId!, exportedName] as const)
-          ?? [];
+            const left: ElementType[] = where?.sync.map((x) => [x, this.moduleId!, exportedName] as const) ?? [];
+            let cur: ElementType | undefined;
 
-      let cur: ElementType | undefined;
+            while ((cur = left.pop())) {
+                const [modId, importedId, exportedName] = cur;
+                const modText = await ModuleCache.getModuleFromNum(modId);
 
-      while ((cur = left.pop())) {
-          const [modId, importedId, exportedName] = cur;
-          const modText = await ModuleCache.getModuleFromNum(modId);
+                if (seen[importedId]?.has(modId)) {
+                    continue;
+                }
+                (seen[importedId] ||= new Set()).add(modId);
+                if (!modText)
+                    continue;
 
-          if (seen[importedId]?.has(modId)) {
-              continue;
-          }
-          (seen[importedId] ||= new Set()).add(modId);
-          if (!modText)
-              continue;
+                const parser = new WebpackAstParser(modText);
+                const uses = parser.getUsesOfImport(importedId, exportedName);
+                const exportedAs = parser.doesReExport(importedId, exportedName);
 
-          const parser = new WebpackAstParser(modText);
-          const uses = parser.getUsesOfImport(importedId, exportedName);
-          const exportedAs = parser.doesReExport(importedId, exportedName);
+                if (exportedAs) {
+                    const where = await parser.getModulesThatRequireThisModule();
 
-          if (exportedAs) {
-              const where = await parser.getModulesThatRequireThisModule();
+                    left.push(...where?.sync.map((x) => [x, parser.moduleId!, exportedAs] as ElementType) ?? []);
+                }
 
-              left.push(...where?.sync.map((x) => [x, parser.moduleId!, exportedAs] as ElementType) ?? []);
-          }
-
-          locs.push(...uses.map((x) => new Location(ModuleCache.getModuleURI(modId), x)));
-      }
+                locs.push(...uses.map((x) => new Location(ModuleCache.getModuleURI(modId), x)));
+            }
         }
         return locs;
     }

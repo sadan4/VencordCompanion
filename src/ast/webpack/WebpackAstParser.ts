702c3d78af309ec972212a9f396bb380786cfce9
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-18 08:21:56 -0400

    crashes

--- src/ast/webpack/WebpackAstParser.ts
+++ src/ast/webpack/WebpackAstParser.ts
@@ -341,38 +341,38 @@ export class WebpackAstParser extends AstParser {
             // the module id that is being searched for uses
             // the ID of the module that exportName will be imported from
             // the exported name to search
-            type ElementType = [
-                moduleId: string,
-                importedId: string,
-                exportName: string | symbol,
-            ];
+            // type ElementType = [
+            //     moduleId: string,
+            //     importedId: string,
+            //     exportName: string | symbol,
+            // ];
 
             const left: ElementType[] = where?.sync.map((x) => [x, this.moduleId!, exportedName] as const) ?? [];
-            let cur: ElementType | undefined;
+            // let cur: ElementType | undefined;
 
-            while ((cur = left.pop())) {
-                const [modId, importedId, exportedName] = cur;
-                const modText = await ModuleCache.getModuleFromNum(modId);
+            // while ((cur = left.pop())) {
+            //     const [modId, importedId, exportedName] = cur;
+            //     const modText = await ModuleCache.getModuleFromNum(modId);
 
-                if (seen[importedId]?.has(modId)) {
-                    continue;
-                }
-                (seen[importedId] ||= new Set()).add(modId);
-                if (!modText)
-                    continue;
+            //     if (seen[importedId]?.has(modId)) {
+            //         continue;
+            //     }
+            //     (seen[importedId] ||= new Set()).add(modId);
+            //     if (!modText)
+            //         continue;
 
-                const parser = new WebpackAstParser(modText);
-                const uses = parser.getUsesOfImport(importedId, exportedName);
-                const exportedAs = parser.doesReExport(importedId, exportedName);
+            //     const parser = new WebpackAstParser(modText);
+            //     const uses = parser.getUsesOfImport(importedId, exportedName);
+            //     const exportedAs = parser.doesReExport(importedId, exportedName);
 
-                if (exportedAs) {
-                    const where = await parser.getModulesThatRequireThisModule();
+            //     if (exportedAs) {
+            //         const where = await parser.getModulesThatRequireThisModule();
 
-                    left.push(...where?.sync.map((x) => [x, parser.moduleId!, exportedAs] as ElementType) ?? []);
-                }
+            //         left.push(...where?.sync.map((x) => [x, parser.moduleId!, exportedAs] as ElementType) ?? []);
+            //     }
 
-                locs.push(...uses.map((x) => new Location(ModuleCache.getModuleURI(modId), x)));
-            }
+            //     locs.push(...uses.map((x) => new Location(ModuleCache.getModuleURI(modId), x)));
+            // }
         }
         // return locs;
     }
--- src/ast/webpack/lsp/ReferenceProvider.ts
+++ src/ast/webpack/lsp/ReferenceProvider.ts
@@ -9,17 +9,17 @@ import { Position, ReferenceProvider as IReferenceProvider, TextDocument, window
 
 export class ReferenceProvider implements IReferenceProvider {
     async provideReferences(document: TextDocument, position: Position): References {
-        if (!isWebpackModule(document.getText()))
-            return;
-        if (!await ModuleCache.hasCache()) {
-            window.showErrorMessage("No Module Cache found, please download modules first");
-            return;
-        }
-        if (!ModuleDepManager.hasModDeps()) {
-            await ModuleDepManager.initModDeps({
-                fromDisk: true,
-            });
-        }
+        // if (!isWebpackModule(document.getText()))
+        //     return;
+        // if (!await ModuleCache.hasCache()) {
+        //     window.showErrorMessage("No Module Cache found, please download modules first");
+        //     return;
+        // }
+        // if (!ModuleDepManager.hasModDeps()) {
+        //     await ModuleDepManager.initModDeps({
+        //         fromDisk: true,
+        //     });
+        // }
         try {
             return await new WebpackAstParser(document.getText())
                 .generateReferences(position);

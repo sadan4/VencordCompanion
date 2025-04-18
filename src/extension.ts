3495907deac0d994a9cda95049f3ffa2b05534f3
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-18 09:06:34 -0400

    still crashes

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,9 @@
 import { ReferenceProvider } from "@ast/webpack/lsp";
-import { outputChannel } from "@modules/logging";
-import { stopWebSocketServer } from "@server";
-
 
 import { ExtensionContext, languages, Uri } from "vscode";
 
-export let extensionUri: Uri;
-export let extensionPath: string;
-
 
 export function activate(context: ExtensionContext) {
-    extensionUri = context.extensionUri;
-    extensionPath = context.extensionPath;
     context.subscriptions.push(
 
         languages.registerReferenceProvider({ language: "javascript" }, new ReferenceProvider()),
@@ -22,9 +14,5 @@ export function activate(context: ExtensionContext) {
 }
 
 export function deactivate() {
-    stopWebSocketServer();
 }
-export {
-    outputChannel,
-};
 

971b1f10d4427a5b6cdd43c57d30591c391c7850
sadan <117494111+sadan4@users.noreply.github.com>
2025-09-17 09:37:40 -0400

    fix test:ui on windows

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
     test: {
         // https://github.com/vitest-dev/vitest/issues/7759#issuecomment-2812533570
         root: import.meta.dirname,
-        projects: ["packages/*"]
+        projects: ["packages/*"],
     }
 })
